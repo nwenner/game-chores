c8f145f68cf2ea3367f2c850a0d963556edca0b6
Nick Wenner <nwenner@gmail.com>
2020-12-18 13:48:29 -0600

    Added react-bootstrap to the build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           
-          Learn React
+          <Button variant="primary">Learn React</Button>
         </a>
       </header>
     </div>
